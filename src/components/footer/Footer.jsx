2b6f56459dae5b06f4ea94b6284c29310ec6794e
Malone-chisom <johnsonmalone007@gmail.com>
2024-07-09 19:34:46 +0100

    now

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                             <div >
                                 <h3>PAYMENTS METHODS</h3>
                                 <div className='footer-left-1'>
-                                    <div className=' ul-1'>
+                                    {/* <div className=' ul-1'>
                                         <div>
                                             <img src={assets.mastercard} alt="" />
                                         </div>
@@ -20,50 +20,50 @@ const Footer = () => {
                                         </div>
                                         <div>
                                             <img src={assets.paypal} alt="" />
-                                        </div>
-                                    </div>
+                                        </div> */}
                                 </div>
+                            </div>
 
+                        </div>
+                        <div>
+                            <h3>SHOP</h3>
+                            <div className='footer-left-2'>
+                                <ul>
+                                    <li>All Collections</li>
+                                    <li>Shop Bags</li>
+                                    <li>Shop Shoes</li>
+                                    <li></li>
+                                </ul>
                             </div>
-                            <div>
-                                <h3>SHOP</h3>
-                                <div className='footer-left-2'>
-                                    <ul>
-                                        <li>All Collections</li>
-                                        <li>Shop Bags</li>
-                                        <li>Shop Shoes</li>
-                                        <li></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div>
-                                <h3>COMPANY</h3>
-                                <div className='footer-left-3'>
-                                    <ul>
-                                        <li>About Us</li>
-                                        <li>Contact Us</li>
-                                    </ul>
-                                </div>
+                        </div>
+                        <div>
+                            <h3>COMPANY</h3>
+                            <div className='footer-left-3'>
+                                <ul>
+                                    <li>About Us</li>
+                                    <li>Contact Us</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div className='footer-right'>
-                        <div className='footer-right-container'>
-                            <p>Subscribe to our Newsletter</p>
-                            <form className='footer-form'>
-                                <input type='text' name='name' placeholder='Name' required />
-                                <input type='email' name='email' placeholder='Email Address' required />
-                                <button type='submit'>Subscribe</button>
-                            </form>
-                        </div>
-
+                <div className='footer-right'>
+                    <div className='footer-right-container'>
+                        <p>Subscribe to our Newsletter</p>
+                        <form className='footer-form'>
+                            <input type='text' name='name' placeholder='Name' required />
+                            <input type='email' name='email' placeholder='Email Address' required />
+                            <button type='submit'>Subscribe</button>
+                        </form>
                     </div>
-
 
                 </div>
+
+
             </div>
         </div>
+        </div >
     )
 }
 
